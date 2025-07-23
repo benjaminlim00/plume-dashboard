6900662a670184428de8ab492bf86bfb64400866
Benjamin Lim <benjaminlim1996@hotmail.sg>
2025-07-24 00:40:26 +0700

    chore: add comment

--- hooks/usePerformanceMonitor.ts
+++ hooks/usePerformanceMonitor.ts
@@ -1,6 +1,10 @@
 import { useState, useCallback, useEffect } from "react"
 import { MATRIX_CONFIG } from "../lib/matrixRain"
 
+// 60 FPS -> 1000ms/60 = 16ms per frame
+// logic: if a simple setTimeout(callback, 16) takes longer than 16ms to execute, it means:
+// Browser can't keep up with 60 FPS, frames are dropping, animation will look choppy
+
 export const usePerformanceMonitor = () => {
   const [isLowPerformance, setIsLowPerformance] = useState(false)
 
